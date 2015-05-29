724dcf4ebed19afd6db68264efad6a27a4267db0
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-05-29 09:30:45 -0700

    adding in some timespan extensions to calculate the ceiling

---
+++ src/Nagger.Services/ExtensionMethods/TimeSpanExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Nagger.Services.ExtensionMethods
+{
+    using System;
+
+    public static class TimeSpanExtensions
+    {
+        public static TimeSpan Ceiling(this TimeSpan original, TimeSpan ceiling)
+        {
+            if (original == ceiling || ceiling.Ticks == 0) return original;
+            var ticks = (original.Ticks + ceiling.Ticks - 1)/ceiling.Ticks;
+            return new TimeSpan(ticks * ceiling.Ticks);
+        }
+    }
+}
