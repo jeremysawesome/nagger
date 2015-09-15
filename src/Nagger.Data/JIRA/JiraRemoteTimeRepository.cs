4d0d88e6d3709035d5b19fe94dd91b2c86ccbd01
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-09-15 10:32:21 -0700

    implement actually posting time to JIRA

--- src/Nagger.Data/JIRA/JiraRemoteTimeRepository.cs
+++ src/Nagger.Data/JIRA/JiraRemoteTimeRepository.cs
@@ -1,40 +1,17 @@
 ﻿namespace Nagger.Data.JIRA
 {
-    using System;
     using API;
+    using DTO;
     using Interfaces;
     using Models;
     using RestSharp;
+    using Services.ExtensionMethods;
 
     public class JiraRemoteTimeRepository : IRemoteTimeRepository
     {
         readonly JiraApi _api;
         readonly BaseJiraRepository _baseJiraRepository;
-        // insert a worklog in Jira
-        /*        {
-            "self": "http://www.example.com/jira/rest/api/2/issue/10010/worklog/10000",
-            "author": {
-                "self": "http://www.example.com/jira/rest/api/2/user?username=fred",
-                "name": "fred",
-                "displayName": "Fred F. User",
-                "active": false
-            },
-            "updateAuthor": {
-                "self": "http://www.example.com/jira/rest/api/2/user?username=fred",
-                "name": "fred",
-                "displayName": "Fred F. User",
-                "active": false
-            },
-            "comment": "I did some work here.",
-            "visibility": {
-                "type": "group",
-                "value": "jira-developers"
-            },
-            "started": "2014-10-28T06:52:53.720+0000",
-            "timeSpent": "3h 20m",
-            "timeSpentSeconds": 12000,
-            "id": "100028"
-        }*/
+      
 
         public JiraRemoteTimeRepository(BaseJiraRepository baseJiraRepository)
         {
@@ -42,24 +19,36 @@
             _api = new JiraApi(_baseJiraRepository.JiraUser, _baseJiraRepository.ApiBaseUrl);
         }
 
+        // needs to post to: /rest/api/2/issue/{issueIdOrKey}/worklog
         public bool RecordTime(TimeEntry timeEntry)
         {
-            // currently looking at: http://www.asp.net/web-api/overview/advanced/calling-a-web-api-from-a-net-client
             // Jira requires a task
             if (!timeEntry.HasTask) return false;
-
-
-            // https://www.example.com/rest/api/latest/search?jql=project%3D%22ProjectName%22
-            // needs to post to: /rest/api/2/issue/{issueIdOrKey}/worklog
-            // todo: implement this instead of just returning false
-            return false;
-
             
+            // jira requires a special format - like ISO 8601 but not quite
+            const string jiraTimeFormat = "yyyy-MM-ddTHH:mm:ss.fffK";
+
+            // jira doesn't like the colon in the ISO 8601 string. so we strip it out.
+            var timeStarted = timeEntry.TimeRecorded.ToString(jiraTimeFormat).ReplaceLastOccurrence(":", "");
+
+            var worklog = new Worklog
+            {
+                comment = timeEntry.Comment ?? "",
+                started = timeStarted,
+                timeSpent = timeEntry.MinutesSpent +"m"
+            };
 
             var post = new RestRequest()
             {
-                Resource = "issue/"+timeEntry.Task.Id+"/worklog"
+                Resource = "issue/"+timeEntry.Task.Id+"/worklog?adjustEstimate=leave",
+                Method = Method.POST,
+                RequestFormat = DataFormat.Json
             };
+
+            post.AddBody(worklog);
+
+            var result = _api.Execute<Worklog>(post);
+            return result != null;
         }
     }
 }
