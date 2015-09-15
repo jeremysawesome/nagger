1223096ee89fb9e90b44e71a182a00c32f5929ba
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-09-15 10:26:08 -0700

    adding a Worklog DTO for transferring time entries to JIRA

---
+++ src/Nagger.Data/JIRA/DTO/Worklog.cs
@@ -0,0 +1,9 @@
+﻿namespace Nagger.Data.JIRA.DTO
+{
+    public class Worklog
+    {
+        public string timeSpent { get; set; }
+        public string started { get; set; }
+        public string comment { get; set; }
+    }
+}
