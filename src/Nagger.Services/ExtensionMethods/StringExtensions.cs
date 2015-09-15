1caf847ad7247ab2dc6c3edef148da736790d5db
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-09-15 10:27:05 -0700

    adding in a string extension for removing the last occurrence of a value in a string

---
+++ src/Nagger.Services/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Nagger.Services.ExtensionMethods
+{
+    using System;
+
+    public static class StringExtensions
+    {
+        //see: http://stackoverflow.com/a/14826068/296889
+        public static string ReplaceLastOccurrence(this string source, string find, string replace)
+        {
+            var place = source.LastIndexOf(find, StringComparison.Ordinal);
+
+            if (place == -1)
+                return string.Empty;
+
+            var result = source.Remove(place, find.Length).Insert(place, replace);
+            return result;
+        }
+    }
+}
