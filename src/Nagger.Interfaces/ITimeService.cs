32592ad03bbaed8d83ae1b9cddf37e4386255efa
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-09-16 07:58:15 -0700

    moving RecordTime position in TimeService interface

--- src/Nagger.Interfaces/ITimeService.cs
+++ src/Nagger.Interfaces/ITimeService.cs
@@ -6,8 +6,8 @@
     public interface ITimeService
     {
         void RecordTime(Task task);
-        void RecordTime(Task task, DateTime time);
         void RecordTime(TimeEntry timeEntry);
+        void RecordTime(Task task, DateTime time);
         void DailyTimeSync();
         void SquashTime(); // this will probably end up being internal to the time service
         void SyncWithRemote();
