90f7b3c3cbbb2990b0f28618ce5827f949d2a353
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-09-17 06:18:17 -0700

    Move the nagger.ndb file to the users app data folder. It's a better place to store it.

--- src/Nagger.Data/LocalBaseRepository.cs
+++ src/Nagger.Data/LocalBaseRepository.cs
@@ -1,14 +1,26 @@
 ﻿namespace Nagger.Data
 {
+    using System;
     using System.Data.SQLite;
+    using System.IO;
 
     public abstract class LocalBaseRepository
     {
         protected SQLiteConnection GetConnection()
         {
+            const string naggerDb = "\\nagger.ndb";
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var dataSource = appData + "\\Nagger\\" + naggerDb;
+
+            if (!File.Exists(dataSource))
+            {
+                Directory.CreateDirectory(appData + "\\Nagger");
+                File.Copy("."+naggerDb, dataSource);
+            }
+
             // this still needs work. the db needs to already exist... it should be setup already.
             // we can take a dependency on it
-            var cnn = new SQLiteConnection("Data Source=.//nagger.ndb");
+            var cnn = new SQLiteConnection("Data Source="+dataSource);
             cnn.Open();
 
             // Encrypts the database. The connection remains valid and usable afterwards.
