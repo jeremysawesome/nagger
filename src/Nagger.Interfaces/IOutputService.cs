01e61f685affa7bcfd7efe9073f66b39c4d4f12c
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-09-16 19:27:41 -0700

    creating an output service that can be used instead of directly accessing the console

---
+++ src/Nagger.Interfaces/IOutputService.cs
@@ -0,0 +1,10 @@
+﻿namespace Nagger.Interfaces
+{
+    public interface IOutputService
+    {
+        void ShowInformation(string information);
+        void LoadingMessage(string message);
+        void HideInterface();
+        void ShowInterface();
+    }
+}
---
+++ src/Nagger.Services/ConsoleOutputService.cs
@@ -0,0 +1,53 @@
+﻿namespace Nagger.Services
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Interfaces;
+
+    public class ConsoleOutputService : IOutputService
+    {
+        internal static class ConsoleUtil
+        {
+            const int Hide = 0;
+            const int Show = 5;
+
+            [DllImport("kernel32.dll")]
+            static extern IntPtr GetConsoleWindow();
+
+            [DllImport("user32.dll")]
+            static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+            public static void HideWindow()
+            {
+                var handle = GetConsoleWindow();
+                ShowWindow(handle, Hide);
+            }
+
+            public static void ShowWindow()
+            {
+                var handle = GetConsoleWindow();
+                ShowWindow(handle, Show);
+            }
+        }
+
+        public void ShowInformation(string information)
+        {
+            Console.WriteLine(information);
+        }
+
+        public void LoadingMessage(string message)
+        {
+            ShowInformation(message);
+        }
+
+        public void HideInterface()
+        {
+            ConsoleUtil.HideWindow();
+        }
+
+        public void ShowInterface()
+        {
+            ConsoleUtil.ShowWindow();
+        }
+    }
+}
