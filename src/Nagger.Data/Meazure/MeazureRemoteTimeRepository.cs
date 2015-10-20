f34eace65fc52df52a3699be3110247674359ce9
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-10-19 23:21:06 -0700

    moving meazure specific notes to the Meazure specific remote time repository implementation

--- src/Nagger.Data/Meazure/MeazureRemoteTimeRepository.cs
+++ src/Nagger.Data/Meazure/MeazureRemoteTimeRepository.cs
@@ -8,6 +8,21 @@
     {
         public bool RecordTime(TimeEntry timeEntry)
         {
+            /**
+             note. Add in an implementation for Meazure.
+             you should be able to post a JSON object to /Time/Save  which will save a single time entry
+             example JSON:
+             {
+                "Date":"2015-10-16T00:00:00.000Z",
+                "Notes":"Example Notes(comments)",
+                "ProjectId":"426", // project
+                "TaskId":"3", // this is more like a "task type"
+                "WorkItems":[], // task
+                "TimeString":"120m",
+                "DurationSeconds":7200
+            }
+             a workitem in Meazure is akin to a issue key in JIRA. 
+            **/
             throw new NotImplementedException();
         }
     }
