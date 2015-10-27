403fef44316480701158f8dae875ebe640d7119b
Jeremy Smith <jeremy.smith@motorcycle-superstore.com>
2015-10-27 11:00:40 -0700

    WIP

--- src/Nagger.Data/Meazure/MeazureRemoteTimeRepository.cs
+++ src/Nagger.Data/Meazure/MeazureRemoteTimeRepository.cs
@@ -1,7 +1,6 @@
 ﻿namespace Nagger.Data.Meazure
 {
     using System;
-    using System.Collections.Generic;
     using DTO;
     using Interfaces;
     using Models;
@@ -34,10 +33,46 @@
                 Date = timeEntry.TimeRecorded.ToString("O"),
                 Notes = timeEntry.Comment,
                 TimeString = timeEntry.MinutesSpent + "m",
-                DurationSeconds = timeEntry.MinutesSpent * 60,
+                DurationSeconds = timeEntry.MinutesSpent*60,
                 ProjectId = timeEntry.Project?.Id,
-                TaskId = timeEntry.Task?.Id // note the task is different in meazure, a task is more of a "task type" in meazure
+                TaskId = timeEntry.Task?.Id
+                // note the task is different in meazure, a task is more of a "task type" in meazure
             };
+
+
+            /*
+            public bool RecordTime(TimeEntry timeEntry)
+        {
+            // Jira requires a task
+            if (!timeEntry.HasTask) return false;
+            
+            // jira requires a special format - like ISO 8601 but not quite
+            const string jiraTimeFormat = "yyyy-MM-ddTHH:mm:ss.fffK";
+
+            // jira doesn't like the colon in the ISO 8601 string. so we strip it out.
+            var timeStarted = timeEntry.TimeRecorded.ToString(jiraTimeFormat).ReplaceLastOccurrence(":", "");
+
+            var worklog = new Worklog
+            {
+                comment = timeEntry.Comment ?? "",
+                started = timeStarted,
+                timeSpent = timeEntry.MinutesSpent +"m"
+            };
+
+            var post = new RestRequest()
+            {
+                Resource = "issue/"+timeEntry.Task.Id+"/worklog?adjustEstimate=leave",
+                Method = Method.POST,
+                RequestFormat = DataFormat.Json
+            };
+
+            post.AddBody(worklog);
+
+            var result = _api.Execute<Worklog>(post);
+            return result != null;
+        }
+            */
+
             throw new NotImplementedException();
         }
     }
