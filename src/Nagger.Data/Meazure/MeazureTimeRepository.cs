7ebee405d8422093df279697dba8640053c5e213
Jeremy Smith <jeremy@jeremysawesome.com>
2016-05-17 15:09:49 -0700

    Begin implementing RecordTime Meazure implementation

--- src/Nagger.Data/Meazure/MeazureTimeRepository.cs
+++ src/Nagger.Data/Meazure/MeazureTimeRepository.cs
@@ -1,5 +1,7 @@
 ﻿namespace Nagger.Data.Meazure
 {
+    using System.Collections.Generic;
+    using DTO;
     using Interfaces;
     using Models;
 
@@ -7,6 +9,22 @@
     {
         public bool RecordTime(TimeEntry timeEntry)
         {
+            // meazure requires either Notes, a project, or a task
+            if (!timeEntry.HasProject && !timeEntry.HasTask && !timeEntry.HasComment) return false;
+
+            var timeEntryModel = new TimeEntryModel
+            {
+                Date = timeEntry.TimeRecorded.ToString("O"),
+                Notes = timeEntry.Comment,
+                TimeString = timeEntry.MinutesSpent + "m",
+                DurationSeconds = timeEntry.MinutesSpent*60,
+                ProjectId = timeEntry.Project?.Id,
+                TaskId = timeEntry.Task?.Id,
+                WorkItems = new List<string>(), //TODO: add functionality for tracking WorkItems
+            };
+
+
+
             throw new System.NotImplementedException();
         }
     }
