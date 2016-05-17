1e16cf6093b276924732965ae2ae4ff39eb433bd
Jeremy Smith <jeremy@jeremysawesome.com>
2016-05-17 15:00:02 -0700

    WIP - Meazure RecordTimeEntry

---
+++ src/Nagger.Data/Meazure/DTO/TimeEntryModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Nagger.Data.Meazure.DTO
+{
+    using System.Collections.Generic;
+
+    public class TimeEntryModel
+    {
+        public string Date { get; set; }
+        public string Notes { get; set; }
+        public string TimeString { get; set; }
+        public string ProjectId { get; set; }
+        public string TaskId { get; set; }
+
+        public int DurationSeconds { get; set; }
+
+        public List<string> WorkItems { get; set; }
+    }
+}
