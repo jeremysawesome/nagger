c5acdcc8a664e50170d15451a3f5177e42a0898f
Jeremy Smith <jeremy@jeremysawesome.com>
2016-05-18 12:13:45 -0700

    WIP! Filling in Known Tasks

--- src/Nagger.Data/Meazure/MeazureTaskRepository.cs
+++ src/Nagger.Data/Meazure/MeazureTaskRepository.cs
@@ -1,29 +1,59 @@
 ﻿namespace Nagger.Data.Meazure
 {
     using System.Collections.Generic;
+    using System.Linq;
     using Interfaces;
     using Models;
+
+    public static class KnownTasks
+    {
+        public static Task Design => BuildTask("4", "Design");
+        public static Task Development => BuildTask("3", "Development");
+        public static Task TestingAndBugFixing => BuildTask("337", "Testing / Bug Fixing");
+        public static Task ProjectManagement => BuildTask("336", "Project Management");
+        public static Task SupportAdmin => BuildTask("345", "Support / Admin");
+        public static Task SalesMarketing => BuildTask("344", "Sales & Marketing");
+        public static Task Vacation => BuildTask("346", "Vacation");
+        public static Task CompanyHoliday => BuildTask("905", "Company Holiday");
+
+        private static Task BuildTask(string id, string name)
+        {
+            return new Task
+            {
+                Id = id,
+                Name = name,
+                Description = name,
+            };
+        }
+    }
 
     public class MeazureTaskRepository : IRemoteTaskRepository
     {
         public Task GetTaskByName(string name)
         {
-            throw new System.NotImplementedException();
+            return GetTasks().First(task => task.Name == name);
         }
 
         public IEnumerable<Task> GetTasks()
         {
-            throw new System.NotImplementedException();
+            yield return KnownTasks.Design;
+            yield return KnownTasks.Development;
+            yield return KnownTasks.TestingAndBugFixing;
+            yield return KnownTasks.ProjectManagement;
+            yield return KnownTasks.SupportAdmin;
+            yield return KnownTasks.SalesMarketing;
+            yield return KnownTasks.Vacation;
+            yield return KnownTasks.CompanyHoliday;
         }
 
         public IEnumerable<Task> GetTasks(Project project)
         {
-            throw new System.NotImplementedException();
+            return GetTasks();
         }
 
         public IEnumerable<Task> GetTasksByProjectId(string projectId, string lastTaskId = null)
         {
-            throw new System.NotImplementedException();
+            return GetTasks();
         }
     }
 }
