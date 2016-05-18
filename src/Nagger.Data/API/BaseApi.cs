e2e3b4891c7e76532b991bf8ab813add239cbca3
Jeremy Smith <jeremy@jeremysawesome.com>
2016-05-18 15:28:49 -0700

    Change to protected fields so that they can be used in derived classes

--- src/Nagger.Data/API/BaseApi.cs
+++ src/Nagger.Data/API/BaseApi.cs
@@ -8,13 +8,13 @@
 
     public class BaseApi
     {
-        readonly string _apiUrl;
-        readonly User _user;
+        protected readonly string ApiUrl;
+        protected readonly User User;
 
         public BaseApi(User user, string apiBaseUrl, string apiPath)
         {
-            _user = user;
-            _apiUrl = apiBaseUrl.TrimEnd('/') + apiPath;
+            User = user;
+            ApiUrl = apiBaseUrl.TrimEnd('/') + apiPath;
         }
 
         public T Execute<T>(RestRequest request, IRestClient client = null) where T : new()
@@ -23,7 +23,7 @@
             var response = client.Execute<T>(request);
 
             // we will deal with this later. When InvalidCredentials are provided then we need to clear out the saved creds
-            if(response.StatusCode == HttpStatusCode.Unauthorized) throw new InvalidCredentialException("User provided is not authorized to access this API: "+_apiUrl);
+            if(response.StatusCode == HttpStatusCode.Unauthorized) throw new InvalidCredentialException("User provided is not authorized to access this API: "+ApiUrl);
 
             if (response.ErrorException == null) return response.Data;
 
@@ -34,8 +34,8 @@
         {
             return new RestClient
             {
-                BaseUrl = new Uri(_apiUrl),
-                Authenticator = new HttpBasicAuthenticator(_user.Username, _user.Password)
+                BaseUrl = new Uri(ApiUrl),
+                Authenticator = new HttpBasicAuthenticator(User.Username, User.Password)
             };
         }
     }
