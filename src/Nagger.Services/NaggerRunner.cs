39fca7ef5fd29b5ae8e0882fda30bdbadc78fdbe
Jeremy Smith <jeremy@jeremysawesome.com>
2016-05-19 08:13:46 -0700

    cleanup unused usings

--- src/Nagger.Services/NaggerRunner.cs
+++ src/Nagger.Services/NaggerRunner.cs
@@ -1,9 +1,7 @@
 ﻿namespace Nagger.Services
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
     using ExtensionMethods;
     using Interfaces;
     using Models;
