86911eb62502235b8f717435c249fa2da34e8001
Jeremy Smith <jeremy@jeremysawesome.com>
2017-08-22 12:49:08 -0700

    add in the fake remote runner

---
+++ src/Nagger.Services/Fake/FakeRunner.cs
@@ -0,0 +1,18 @@
+﻿namespace Nagger.Services.Fake
+{
+    using Interfaces;
+    using Models;
+
+    public class FakeRunner : IRemoteRunner
+    {
+        public Task AskForTask()
+        {
+            return null;
+        }
+
+        public Task AskForAssociatedTask(Task currentTask)
+        {
+            return null;
+        }
+    }
+}
