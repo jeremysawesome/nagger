028de3b869eed9bf9cf0c60b0f4f1e10ca7c9c34
Jeremy Smith <jeremy@jeremysawesome.com>
2017-08-22 12:47:27 -0700

    add in the fake time repo

---
+++ src/Nagger.Data/Fake/FakeTimeRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Nagger.Data.Fake
+{
+    using Interfaces;
+    using Models;
+
+    public class FakeTimeRepository : IRemoteTimeRepository
+    {
+        public bool RecordTime(TimeEntry timeEntry)
+        {
+            return true;
+        }
+
+        public bool RecordAssociatedTime(TimeEntry timeEntry)
+        {
+            return true;
+        }
+
+        public void InitializeForProject(Project project)
+        {
+        }
+    }
+}
