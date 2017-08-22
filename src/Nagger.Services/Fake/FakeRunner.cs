37791f473095ca271f323039078414e78934892b
Jeremy Smith <jeremy@jeremysawesome.com>
2017-08-22 13:15:01 -0700

    update the fakerunner to use the fake development task

--- src/Nagger.Services/Fake/FakeRunner.cs
+++ src/Nagger.Services/Fake/FakeRunner.cs
@@ -1,13 +1,26 @@
 ﻿namespace Nagger.Services.Fake
 {
+    using System.Linq;
     using Interfaces;
     using Models;
 
     public class FakeRunner : IRemoteRunner
     {
+        readonly ITaskService _taskService;
+        readonly IOutputService _outputService;
+
+        public FakeRunner(ITaskService taskService, IOutputService outputService)
+        {
+            _taskService = taskService;
+            _outputService = outputService;
+        }
+
         public Task AskForTask()
         {
-            return null;
+            // fake runner just use the first general task always
+            var task = _taskService.GetGeneralTasks().First();
+            _outputService.ShowInformation($"You are working on {task.Name}.");
+            return task;
         }
 
         public Task AskForAssociatedTask(Task currentTask)
