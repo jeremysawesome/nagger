182aaeb69718904bd505cda6ba34bc2c4c833cf8
Jeremy Smith <jeremy@jeremysawesome.com>
2017-08-22 12:46:14 -0700

    rename FakeRemoteTaskRepository to FakeTaskRepository

---
+++ src/Nagger.Data/Fake/FakeTaskRepository.cs
@@ -0,0 +1,46 @@
+﻿namespace Nagger.Data.Fake
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Interfaces;
+    using Models;
+
+    public class FakeTaskRepository : IRemoteTaskRepository
+    {
+        public Task GetTaskByName(string name)
+        {
+            return null;
+        }
+
+        public IEnumerable<Task> GetTasks()
+        {
+            yield break;
+        }
+
+        public IEnumerable<Task> GetTasks(Project project)
+        {
+            yield break;
+        }
+
+        public IEnumerable<Task> GetTasksByProjectId(string projectId, string lastTaskId = "")
+        {
+            yield break;
+        }
+
+        public void InitializeForProject(Project project)
+        {
+            
+        }
+
+        static Task GetTask(string id, string name, Task parent = null)
+        {
+            return new Task
+            {
+                Id = id,
+                Name = name,
+                Parent = parent
+            };
+        }
+    }
+}
