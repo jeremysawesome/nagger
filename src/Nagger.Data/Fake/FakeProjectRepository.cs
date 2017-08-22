3c6eb0d7abc625ed9479bce4fc5271a5f636890a
Jeremy Smith <jeremy@jeremysawesome.com>
2017-08-22 12:45:12 -0700

    implement a fake project repo

---
+++ src/Nagger.Data/Fake/FakeProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Nagger.Data.Fake
+{
+    using System.Collections.Generic;
+    using Interfaces;
+    using Models;
+
+    public class FakeProjectRepository : IRemoteProjectRepository
+    {
+        public IEnumerable<Project> GetProjects()
+        {
+            yield break;
+        }
+    }
+}
